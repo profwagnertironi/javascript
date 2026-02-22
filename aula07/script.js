fcf45f4ff0b051691024b32fc785b6caaad4710b
Prof. Wagner Tironi Pinto <profwagnertironi@gmail.com>
2026-02-22 23:11:40 +0000

    Criação da calculadora com função

--- aula07/script.js
+++ aula07/script.js
@@ -1,1 +1,32 @@
-console.log('Testes ')
+let btn_soma = document.querySelector("#btn-soma")
+let campo = document.querySelector("#inp-number")
+let mensagem = document.querySelector("#mensagem")
+
+let numeros = []
+
+btn_soma.addEventListener("click", () =>{
+   //addEventListener("click", () =>{
+   console.log('soma')
+})
+
+campo.addEventListener('input', (e) =>{
+   let numero = e.target.value 
+   //document.getElementById("inp-number")
+   console.log('numero ' + numero)
+   numeros[numeros.length] = numero
+})
+
+
+campo.addEventListener('keydown', (e) =>{
+   //let numero = e.target.value 
+   //document.getElementById("inp-number")
+   //console.log('numero ' + numero)
+   if (e.key === 'Enter') {
+      console.log('Enter ' + numeros[numeros.length-1])
+      campo.value = ''
+   }
+})
+//mensagem.innerHTML = "<p>"+numeros+"</p>"
+console.log(numeros)
+
+
